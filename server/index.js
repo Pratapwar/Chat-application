e5c498de2d8541d74d5e9d0c9181c4cd62cdc080
Pratapwar <akhileshpratapwar123@gmail.com>
2024-07-23 19:52:38 +0530

    Removed cors error

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3002",
+    origin: "https://talktomates.netlify.app",
     credentials: true,
   },
 });
